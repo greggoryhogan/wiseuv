f90fda8e037231ba7cb493d3f9db86d76691bffa
Gregg Hogan <21093023+greggoryhogan@users.noreply.github.com>
2023-03-22 12:09:01 -0400

    Update call btn

--- wp-content/plugins/wiseuv/templates/flexible-content/call_wise.php
+++ wp-content/plugins/wiseuv/templates/flexible-content/call_wise.php
@@ -4,10 +4,11 @@ $tag = get_sub_field('tag');
 $size = get_sub_field('size');
 $font_weight = get_sub_field('font_weight'); 
 $tel = get_option('contact_crisis_line_number');
+$crisis_number = '+1'.str_replace('-','',get_option('contact_crisis_line_number'));
 $button_text = get_sub_field('button_text'); ?>
 <div class="flexible-content heading call_wise">
     <?php if($heading != '') {
         echo '<'.$tag.' class="'.$size.' font-weight-'.$font_weight.'">'.wise_content_filters($heading).'</'.$tag.'>';
     } ?>
-    <a href="tel:<?php echo $tel; ?>" class="btn"><?php echo $button_text; ?></a>
+    <a href="tel:<?php echo $crisis_number; ?>" class="btn"><?php echo $button_text; ?></a>
 </div>
