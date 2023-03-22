a581ff8ef08df78732157a2bf3116d405ccea31a
Gregg Hogan <21093023+greggoryhogan@users.noreply.github.com>
2023-03-22 12:04:02 -0400

    Change hoverintent timing

--- wp-content/themes/wiseuv/assets/js/site.js
+++ wp-content/themes/wiseuv/assets/js/site.js
@@ -27,7 +27,7 @@
     //Menu hoverintent config
     var config = {
       sensitivity: 6,
-      timeout: 150,
+      timeout: 250,
       over : function(e){ 
         if(windowWidth > tabletWidth) { //74 rem, defined in variables.scss
           $( this ).addClass( 'is-active' );  
