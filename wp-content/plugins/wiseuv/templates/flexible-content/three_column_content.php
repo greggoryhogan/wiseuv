58dbaeb5436bd0fa19bd4c523ea13341e835bc3c
Gregg Hogan <21093023+greggoryhogan@users.noreply.github.com>
2023-04-19 12:50:50 -0400

    Add 3 column layout

---
+++ wp-content/plugins/wiseuv/templates/flexible-content/three_column_content.php
@@ -0,0 +1,122 @@
+<?php 
+$image = get_sub_field('image'); 
+$heading = get_sub_field( 'heading' );
+$subheading = get_sub_field( 'subheading' );
+$eyebrow = get_sub_field('eyebrow');
+$column_content = get_sub_field('content');
+$link = get_sub_field('cta');
+$image2 = get_sub_field('image_2'); 
+$heading2 = get_sub_field( 'heading_2' );
+$eyebrow2 = get_sub_field('eyebrow_2');
+$subheading2 = get_sub_field( 'subheading_2' );
+$column_content2 = get_sub_field('content_2');
+$link2 = get_sub_field('cta_2');
+$image3 = get_sub_field('image_3'); 
+$heading3 = get_sub_field( 'heading_3' );
+$eyebrow3 = get_sub_field('eyebrow_3');
+$subheading3 = get_sub_field( 'subheading_3' );
+$column_content3 = get_sub_field('content_3');
+$link3 = get_sub_field('cta_3');
+$image_size = get_sub_field('image_size');
+$force_images_full_width = get_sub_field('force_images_full_width');
+$row_order = get_sub_field('row_order');
+$vertical_align = get_sub_field('vertical_align');
+$heading_type = get_sub_field('heading_type');
+$font_size = get_sub_field('font_size');
+$font_weight = get_sub_field('font_weight');
+$image_style = get_sub_field('image_style');
+$column_width = get_sub_field('column_width');
+?>
+<div class="flexible-content three-column-content"><?php
+    
+    echo '<div class="contain-content">';
+        if($image) {
+            echo '<div class="image '.$force_images_full_width.' '.$image_style;
+            if(!$link && $heading == '' && $column_content == '') {
+                echo ' no-margin';
+            }
+            echo '">';
+                echo wp_get_attachment_image( $image, $image_size );
+            echo '</div>';
+        }
+        if($eyebrow != '') {
+            echo '<p class="eyebrow">'.wise_content_filters($eyebrow).'</p>';
+        }
+        if($heading != '') {
+            echo '<'.$heading_type.' class="'.$font_size.' font-weight-'.$font_weight.'">'.wise_content_filters($heading).'</'.$heading_type.'>';
+        }
+        if($subheading != '') {
+            echo '<p class="subheading bodoni">'.wise_content_filters($subheading).'</p>';
+        }
+        if($column_content != '') {
+            echo wise_content_filters($column_content);
+        }
+        if( $link ): 
+            $link_url = $link['url'];
+            $link_title = $link['title'];
+            $link_target = $link['target'] ? $link['target'] : '_self';
+            echo '<a class="btn" href="'.esc_url( $link_url ).'" target="'.esc_attr( $link_target ).'">'.wise_content_filters(esc_html( $link_title )).'</a>';
+        endif;
+    echo '</div>';  
+
+    echo '<div class="contain-content">';
+        if($image2 && $image_style) {
+            echo '<div class="image '.$force_images_full_width.' '.$image_style;
+            if(!$link2 && $heading2 == '' && $column_content2 == '') {
+                echo ' no-margin';
+            }
+            echo '">';
+                echo wp_get_attachment_image( $image2, $image_size );
+            echo '</div>';
+        }
+        if($eyebrow2 != '') {
+            echo '<p class="eyebrow">'.wise_content_filters($eyebrow2).'</p>';
+        }
+        if($heading2 != '') {
+            echo '<'.$heading_type.' class="'.$font_size.'" font-weight-'.$font_weight.'>'.wise_content_filters($heading2).'</'.$heading_type.'>';
+        }
+        if($subheading2 != '') {
+            echo '<p class="subheading bodoni">'.wise_content_filters($subheading2).'</p>';
+        }
+        if($column_content2 != '') {
+            echo wise_content_filters($column_content2);
+        }
+        if( $link2 ): 
+            $link_url = $link2['url'];
+            $link_title = $link2['title'];
+            $link_target = $link2['target'] ? $link2['target'] : '_self';
+            echo '<a class="btn" href="'.esc_url( $link_url ).'" target="'.esc_attr( $link_target ).'">'.wise_content_filters(esc_html( $link_title )).'</a>';
+        endif;
+    echo '</div>';  
+
+    echo '<div class="contain-content">';
+        if($image3 && $image_style) {
+            echo '<div class="image '.$force_images_full_width.' '.$image_style;
+            if(!$link3 && $heading3 == '' && $column_content3 == '') {
+                echo ' no-margin';
+            }
+            echo '">';
+                echo wp_get_attachment_image( $image3, $image_size );
+            echo '</div>';
+        }
+        if($eyebrow3 != '') {
+            echo '<p class="eyebrow">'.wise_content_filters($eyebrow3).'</p>';
+        }
+        if($heading3 != '') {
+            echo '<'.$heading_type.' class="'.$font_size.'" font-weight-'.$font_weight.'>'.wise_content_filters($heading3).'</'.$heading_type.'>';
+        }
+        if($subheading3 != '') {
+            echo '<p class="subheading bodoni">'.wise_content_filters($subheading3).'</p>';
+        }
+        if($column_content3 != '') {
+            echo wise_content_filters($column_content3);
+        }
+        if( $link3 ): 
+            $link_url = $link3['url'];
+            $link_title = $link3['title'];
+            $link_target = $link3['target'] ? $link3['target'] : '_self';
+            echo '<a class="btn" href="'.esc_url( $link_url ).'" target="'.esc_attr( $link_target ).'">'.wise_content_filters(esc_html( $link_title )).'</a>';
+        endif;
+    echo '</div>';  
+    ?>
+</div>
