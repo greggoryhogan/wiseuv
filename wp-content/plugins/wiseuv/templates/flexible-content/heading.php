e59e0bc2c8ed1b7a60a8bcf3f7dcd068aaade06f
Gregg Hogan <21093023+greggoryhogan@users.noreply.github.com>
2023-04-26 14:37:45 -0400

    Remove color from heading

--- wp-content/plugins/wiseuv/templates/flexible-content/heading.php
+++ wp-content/plugins/wiseuv/templates/flexible-content/heading.php
@@ -2,10 +2,9 @@
 $heading = get_sub_field( 'heading' );
 $tag = get_sub_field('tag');
 $size = get_sub_field('size');
-$font_weight = get_sub_field('font_weight');
-$color = get_sub_field('color'); ?>
+$font_weight = get_sub_field('font_weight'); ?>
 <div class="flexible-content heading">
     <?php if($heading != '') {
-        echo '<'.$tag.' class="'.$size.' font-weight-'.$font_weight.' color-'.$color.'">'.wise_content_filters($heading).'</'.$tag.'>';
+        echo '<'.$tag.' class="'.$size.' font-weight-'.$font_weight.'">'.wise_content_filters($heading).'</'.$tag.'>';
     } ?>
 </div>
