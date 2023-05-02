cfe8f3cc458e765ec256953697d6419839b14c15
Gregg Hogan <21093023+greggoryhogan@users.noreply.github.com>
2023-05-02 14:20:06 -0400

    Add flexible grid

---
+++ wp-content/plugins/wiseuv/templates/flexible-content/flexible_grid.php
@@ -0,0 +1,38 @@
+<?php 
+$tag = get_sub_field('tag');
+$size = get_sub_field('size');
+$font_weight = get_sub_field('font_weight');
+if(have_rows('columns')) {
+    echo '<div class="flexible-content flexible-grid">';
+        while(have_rows('columns')) {
+            the_row();
+            echo '<div class="flexible-column">';
+            if(have_rows('rows')) {
+                while(have_rows('rows')) {
+                    the_row();
+                    $heading = get_sub_field( 'heading' );
+                    $content = get_sub_field('content'); ?>
+                    <div class="row-content">
+                        <?php if($heading != '') {
+                            echo '<'.$tag.' class="'.$size.' font-weight-'.$font_weight.'">'.wise_content_filters($heading).'</'.$tag.'>';
+                        }
+                        if($content != '') {
+                            echo $content;
+                        }
+                        echo '<div class="flexible-content cta-buttons columns-auto">';
+                            $link = get_sub_field('button');
+                            if( $link ): 
+                                $link_url = $link['url'];
+                                $link_title = $link['title'];
+                                $link_target = $link['target'] ? $link['target'] : '_self';
+                                echo '<a class="btn" href="'.esc_url( $link_url ).'" target="'.esc_attr( $link_target ).'">'.wise_content_filters(esc_html( $link_title )).'</a>';
+                            endif;
+                        echo '</div>';
+                        ?>
+                    </div><?php 
+                }
+            }
+            echo '</div>';
+        }
+    echo '</div>';
+}
