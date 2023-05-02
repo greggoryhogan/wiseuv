602e737dfb3fb5303ce1936a857280622759e350
Gregg Hogan <21093023+greggoryhogan@users.noreply.github.com>
2023-05-02 13:55:36 -0400

    Add styling to headings

--- wp-content/plugins/wiseuv/templates/flexible-content/heading-wysiwyg-repeater.php
+++ wp-content/plugins/wiseuv/templates/flexible-content/heading-wysiwyg-repeater.php
@@ -1,20 +1,22 @@
 <?php 
 if(have_rows('sections')) {
-    while(have_rows('sections')) {
-        the_row();
-        $heading = get_sub_field( 'heading' );
-        $tag = get_sub_field('tag');
-        $size = get_sub_field('size');
-        $font_weight = get_sub_field('font_weight');
-        $content = get_sub_field('content'); ?>
-        <div class="flexible-content heading">
-            <?php if($heading != '') {
-                echo '<'.$tag.' class="'.$size.' font-weight-'.$font_weight.'">'.wise_content_filters($heading).'</'.$tag.'>';
-            }
-            if($content != '') {
-                echo $content;
-            }
-            ?>
-        </div><?php 
-    }
+    echo '<div class="flexible-content heading-wysiwyg-repeater">';
+        while(have_rows('sections')) {
+            the_row();
+            $heading = get_sub_field( 'heading' );
+            $tag = get_sub_field('tag');
+            $size = get_sub_field('size');
+            $font_weight = get_sub_field('font_weight');
+            $content = get_sub_field('content'); ?>
+            <div class="heading-w-content">
+                <?php if($heading != '') {
+                    echo '<'.$tag.' class="'.$size.' font-weight-'.$font_weight.'">'.wise_content_filters($heading).'</'.$tag.'>';
+                }
+                if($content != '') {
+                    echo $content;
+                }
+                ?>
+            </div><?php 
+        }
+    echo '</div>';
 }
