be078fbc19d16b6b8771f46630c3b8ae9528b932
Gregg Hogan <21093023+greggoryhogan@users.noreply.github.com>
2023-05-02 13:59:07 -0400

    Add link to heading wysiyg

--- wp-content/plugins/wiseuv/templates/flexible-content/heading-wysiwyg-repeater.php
+++ wp-content/plugins/wiseuv/templates/flexible-content/heading-wysiwyg-repeater.php
@@ -15,6 +15,15 @@ if(have_rows('sections')) {
                 if($content != '') {
                     echo $content;
                 }
+                echo '<div class="flexible-content cta-buttons columns-auto">';
+                    $link = get_sub_field('button');
+                    if( $link ): 
+                        $link_url = $link['url'];
+                        $link_title = $link['title'];
+                        $link_target = $link['target'] ? $link['target'] : '_self';
+                        echo '<a class="btn" href="'.esc_url( $link_url ).'" target="'.esc_attr( $link_target ).'">'.wise_content_filters(esc_html( $link_title )).'</a>';
+                    endif;
+                echo '</div>';
                 ?>
             </div><?php 
         }
