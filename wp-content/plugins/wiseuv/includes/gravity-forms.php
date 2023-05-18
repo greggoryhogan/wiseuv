b4eca9d472bef17e0b1a889acbff25c7dcc2fe1b
Gregg Hogan <21093023+greggoryhogan@users.noreply.github.com>
2023-05-18 18:27:46 -0400

    Add gform anchor

--- wp-content/plugins/wiseuv/includes/gravity-forms.php
+++ wp-content/plugins/wiseuv/includes/gravity-forms.php
@@ -4,3 +4,8 @@
  */
 require_once( WISE_PLUGIN_DIR . 'includes/gravity-forms-field-types/field_references.php' );
 require_once( WISE_PLUGIN_DIR . 'includes/gravity-forms-field-types/field_message.php' );
+
+/**
+ * Add anchor to all forms
+ */
+add_filter( 'gform_confirmation_anchor', '__return_true' );
