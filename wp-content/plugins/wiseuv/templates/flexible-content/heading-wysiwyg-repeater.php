b562830d8f62a9f01b29b7653c70ec5cd7412698
Gregg Hogan <21093023+greggoryhogan@users.noreply.github.com>
2023-06-20 10:57:49 -0400

    Add links to heading

--- wp-content/plugins/wiseuv/templates/flexible-content/heading-wysiwyg-repeater.php
+++ wp-content/plugins/wiseuv/templates/flexible-content/heading-wysiwyg-repeater.php
@@ -7,20 +7,28 @@ if(have_rows('sections')) {
             $tag = get_sub_field('tag');
             $size = get_sub_field('size');
             $font_weight = get_sub_field('font_weight');
-            $content = get_sub_field('content'); ?>
+            $content = get_sub_field('content');
+            $link = get_sub_field('button');
+            if( $link ): 
+                $link_url = $link['url'];
+                $link_title = $link['title'];
+                $link_target = $link['target'] ? $link['target'] : '_self';
+            endif; ?>
             <div class="heading-w-content">
                 <?php if($heading != '') {
+                    if( $link ): 
+                        echo '<a href="'.esc_url( $link_url ).'" target="'.esc_attr( $link_target ).'">';
+                    endif;
                     echo '<'.$tag.' class="'.$size.' font-weight-'.$font_weight.'">'.wise_content_filters($heading).'</'.$tag.'>';
+                    if( $link ): 
+                        echo '</a>';
+                    endif;
                 }
                 if($content != '') {
                     echo $content;
                 }
                 echo '<div class="flexible-content cta-buttons columns-auto">';
-                    $link = get_sub_field('button');
                     if( $link ): 
-                        $link_url = $link['url'];
-                        $link_title = $link['title'];
-                        $link_target = $link['target'] ? $link['target'] : '_self';
                         echo '<a class="btn" href="'.esc_url( $link_url ).'" target="'.esc_attr( $link_target ).'">'.wise_content_filters(esc_html( $link_title )).'</a>';
                     endif;
                 echo '</div>';
--- wp-content/plugins/wiseuv/templates/flexible-content/heading.php
+++ wp-content/plugins/wiseuv/templates/flexible-content/heading.php
@@ -2,9 +2,16 @@
 $heading = get_sub_field( 'heading' );
 $tag = get_sub_field('tag');
 $size = get_sub_field('size');
-$font_weight = get_sub_field('font_weight'); ?>
+$font_weight = get_sub_field('font_weight');
+$heading_links_to = get_sub_field('heading_links_to'); ?>
 <div class="flexible-content heading">
     <?php if($heading != '') {
+        if($heading_links_to != '') {
+            echo '<a href="'.$heading_links_to.'">';
+        }
         echo '<'.$tag.' class="'.$size.' font-weight-'.$font_weight.'">'.wise_content_filters($heading).'</'.$tag.'>';
+        if($heading_links_to != '') {
+            echo '</a>';
+        }
     } ?>
 </div>
